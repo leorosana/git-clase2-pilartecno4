588af37f209b0e6662f2cbda737e46efd9f8be45
leorosana <leorosana@gmail.com>
2023-06-20 19:16:29 -0300

    refactor: changed cow variable name

--- cowsay.js
+++ cowsay.js
@@ -1,7 +1,7 @@
 const cowsay = require("cowsay");
-const cow = cowsay.say({
+const message = cowsay.say({
     text : "Hola Pilar Tecno #4 - Soy una vaquita",
     e : "oO",
     T : "U "
 });
-console.log(cow);
+console.log(message);
