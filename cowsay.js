28ca20badd5b89bdd9e59022bc341fc2610ea658
leorosana <leorosana@gmail.com>
2023-06-20 17:53:40 -0300

    feature:added cow message

--- cowsay.js
+++ cowsay.js
@@ -1,1 +1,7 @@
-console.log('WIP Cowsay - project');
+const cowsay = require("cowsay");
+const cow = cowsay.say({
+    text : "Hola Pilar Tecno #4 - Soy una vaquita",
+    e : "oO",
+    T : "U "
+});
+console.log(cow);
